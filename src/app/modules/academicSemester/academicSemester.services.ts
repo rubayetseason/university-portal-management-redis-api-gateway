24e339bbea297e118f6059528fb0beb2c79bcb0a
rubayet-islam <season.ss2001@gmail.com>
2023-09-21 15:31:00 +0600

    IGeneric response added to promise return type

--- src/app/modules/academicSemester/academicSemester.services.ts
+++ src/app/modules/academicSemester/academicSemester.services.ts
@@ -1,9 +1,10 @@
 import { Request } from 'express';
 import { CoreService as HttpService } from '../../../shared/axios';
+import { IGenericResponse } from '../../../interfaces/common';
 
-const createSemester = async (req: Request) => {
+const createSemester = async (req: Request): Promise<IGenericResponse> => {
   //academicSemester is more needed in coreService hence use core service here
-  const response = await HttpService.post('/academic-semester', req.body, {
+  const response: IGenericResponse = await HttpService.post('/academic-semester', req.body, {
     headers: {
       Authorization: req.headers.authorization
     }
