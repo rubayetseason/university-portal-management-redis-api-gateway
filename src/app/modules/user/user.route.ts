9e365fccfe80be7fc13010baf2dd3d9f727597bb
rubayet-islam <season.ss2001@gmail.com>
2023-09-25 17:03:31 +0600

    multer used to handle file upload

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,8 +1,20 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import { UserController } from './user.controller';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import { FileUploadHelper } from '../../../helpers/FileUploadHelper';
+import { UserValidation } from './user.validation';
 
 const router = express.Router();
 
-router.post('/create-student', UserController.createStudent)
+router.post(
+  '/create-student',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  FileUploadHelper.upload.single('file'),
+  (req: Request, res: Response, next: NextFunction) => {
+    req.body = UserValidation.createZodStudent.parse(JSON.parse(req.body.data));
+    return UserController.createStudent(req, res, next);
+  }
+);
 
 export const UserRoutes = router;
--- src/helpers/FileUploadHelper.ts
+++ src/helpers/FileUploadHelper.ts
@@ -1,10 +1,22 @@
 import { v2 as cloudinary } from 'cloudinary';
+import multer from 'multer';
 
 cloudinary.config({
   cloud_name: 'doi2yyvnj',
   api_key: '168459993363291',
   api_secret: 'cWe3f962nFJN62LxSZ755WuwZ8s'
 });
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
 
 const uploadToCloudinary = async () => {
   cloudinary.uploader.upload(
@@ -16,4 +28,4 @@ const uploadToCloudinary = async () => {
   );
 };
 
-export const FileUploadHelper = { uploadToCloudinary };
+export const FileUploadHelper = { uploadToCloudinary, upload };
